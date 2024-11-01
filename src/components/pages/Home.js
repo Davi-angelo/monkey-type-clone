b8aca1024c19ff395de9ca5ae655bfac1db10d08
Davi Angelo <davi.magalhaes.pb@compasso.com.br>
2024-11-01 11:52:48 -0300

    Texto pra div

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,15 +1,17 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import styles from "./Home.module.css"
 
 function Home() {
+    const API_KEY = process.env.REACT_APP_API_KEY
     const [text, setText] = useState("teste")
+
 
     function getWord(){
 
         fetch("https://api.api-ninjas.com/v1/randomword", {
             method: "GET",
             headers: {
-                'X-Api-Key': '9RbaL5Ton+JVija+kr+8Hg==cHLTEVmRbPthKlWN',
+                'X-Api-Key': API_KEY,
                 "Content-type": "application/json"
             },
         })
@@ -21,12 +23,39 @@ function Home() {
 
     }
 
+    function changeText(){
+        const changedText = document.getElementById("textArea").value
+        let newText = text
+        
+        setText(newText[0]) 
+    }
+
+    function geraDiv(text){
+        let i = 0
+        let letterList = []
+        for(i; i < text.length; i++){
+            letterList.push(<letter key={i}>{text[i]}</letter>)
+        }
+        return letterList
+    }
+
 
     return (
         <div className={styles.page}>
             <div className={styles.calculator}>
 
+                <textarea id="textArea" autoFocus onInput={changeText}>
+                    
+                </textarea>
+
                 <p onClick={getWord}>{text}</p>
+
+                <div>
+                    {
+                        geraDiv(text)
+                    }
+                </div>
+                
 
             </div>
         </div>
